91a24b2579bee4fd801134a999f03d6d92aede26
chain <klaus.mika1052@gmail.com>
2023-08-20 14:28:01 -0300

    Testes automatizados - Categorizando os testes

--- test/transaction.spec.ts
+++ test/transaction.spec.ts
@@ -1,22 +1,24 @@
-import { test, beforeAll, afterAll } from 'vitest'
+import { it, describe, beforeAll, afterAll } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
-beforeAll(async () => {
-  await app.ready()
-})
+describe('Transactions', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
 
-afterAll(async () => {
-  await app.close()
-})
+  afterAll(async () => {
+    await app.close()
+  })
 
-test('User can create a new transaction.', async () => {
-  await request(app.server)
-    .post('/transactions')
-    .send({
-      title: 'New transaction',
-      amount: 5000,
-      type: 'credit',
-    })
-    .expect(201)
+  it('should be able to create a new transaction.', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
 })
